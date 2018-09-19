4d90ead5205ebeaa6353f597866f369087ca22fe
Thiago Braga <thibraga06@gmail.com>
2018-09-19 17:17:18 -0300

    Still without ideas, testing header font
    I'm having a writer's block while I'm designing my website. I'm still
    running out of ideas of what to put on the site, so I'm testing some
    style changes while learning more from React.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">Thiago Braga</div>
+      <div className="App-header">Thiago Braga</div>
     );
   }
 }
