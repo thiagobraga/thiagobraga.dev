e403ce9690ce1fb49d1182f7657e1950e6482aa8
Thiago Braga <thibraga06@gmail.com>
2020-04-26 18:57:57 -0300

    feat: Navbar and sections improvements

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Loader from './components/Loader'
 import Logo from './components/Logo/Logo'
 import Navbar from './components/Navbar'
+import { Github } from '@icons-pack/react-simple-icons'
 import './styles/layout.sass'
 import './styles/typography.sass'
 import './styles/components/sections.sass'
@@ -14,13 +15,24 @@ class App extends Component {
 
         <Navbar>
           <Logo />
+
+          <a
+            href="https://github.com/thiagobraga"
+            target="blank"
+            rel="noopener noreferrer">
+            <Github />
+          </a>
         </Navbar>
 
-        <section className="first">
+        <section className="home">
           <Logo large />
 
           <p>Developer</p>
           <p>Musician</p>
+        </section>
+
+        <section className="about">
+          <h1>Whoami</h1>
         </section>
       </div>
     )
