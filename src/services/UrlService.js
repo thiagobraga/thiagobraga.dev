c4d3f14e0bd13865f15f60c8a310030218424eff
Thiago Braga <thibraga06@gmail.com>
2022-03-01 22:51:51 -0300

    feat: New UrlService.js

---
+++ src/services/UrlService.js
@@ -0,0 +1,17 @@
+const url = {
+  tesseract:     'https://grupotesseract.com.br',
+  sociopata:     'https://open.spotify.com/artist/4jThTw7lss5OnXBzwXYFSD',
+  revel:         'https://open.spotify.com/artist/6mDkruhCYigOgLPIH9JJ0K',
+  onemanbraga:   'https://www.notion.so/thiagobraga/One-Man-Braga-7b5bf719ea4d44cbb2bea165f6bb1fa6',
+  stackoverflow: 'https://stackoverflow.com/users/1096219/thiagobraga',
+  linkedin:      'https://www.linkedin.com/in/thiago-braga',
+  instagram:     'https://instagram.com/thiagobraga.dev',
+  facebook:      'https://facebook.com/thiagobraga.dev',
+  twitter:       'https://twitter.com/thiagobragadev',
+  codepen:       'https://codepen.io/thiagobraga',
+  github:        'https://github.com/thiagobraga',
+  gitlab:        'https://gitlab.com/thiagobraga',
+  devto:         'https://dev.to/thiagobraga',
+};
+
+export default url;
