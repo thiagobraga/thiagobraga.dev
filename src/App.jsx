d950e338aa76f2c4bedfd2c007e56d91cc8a759c
Thiago Braga <thibraga06@gmail.com>
2022-03-01 22:50:42 -0300

    feat: New App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import Scrollbar from 'smooth-scrollbar';
+import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Section/Home';
+import Skills from './components/Section/Skills';
+import WhoAmI from './components/Section/WhoAmI';
+import './styles/components/scrollbar.sass';
+import './styles/components/sections.sass';
+import './styles/typography.sass';
+import './styles/layout.sass';
+
+export default function App() {
+  const [offsetY, setOffsetY] = useState(0);
+  const [limitY, setLimitY] = useState(false);
+
+  useEffect(() => {
+    const scrollbar = Scrollbar.init(window.document.querySelector('[data-scrollbar]'));
+    const onScroll = (status) => {
+      setOffsetY(status.offset.y);
+      setLimitY(status.limit.y);
+    };
+
+    scrollbar.addListener(onScroll);
+    return () => scrollbar.removeListener(onScroll);
+  }, [offsetY, limitY]);
+
+  // Dynamic opacity of the navigation bar
+  const navbarOpacity = offsetY > 150 ? offsetY * 0.15 * 0.01 : 0.15;
+  const navbarBackground = `rgba(12, 13, 17, ${Math.min(navbarOpacity, 1)})`;
+
+  // Parallax effect applied to background images
+  const headerParallax = -(offsetY * 0.7);
+  const footerParallax = (limitY - offsetY - 50) * 0.7;
+
+  // Define styles to use in components
+  const styles = {
+    headerParallax: { backgroundPositionY: headerParallax },
+    footerParallax: { backgroundPositionY: footerParallax },
+    navbar: { backgroundColor: navbarBackground },
+  };
+
+  return (
+    <>
+      <Navbar style={styles.navbar} />
+
+      <main data-scrollbar>
+        <Home style={styles.headerParallax} />
+        <WhoAmI />
+        <Skills />
+
+        <Footer style={styles.footerParallax} />
+      </main>
+    </>
+  );
+}
