27c33e7314da15f910c9675c97489e4c55cf37d1
Thiago Braga <thibraga06@gmail.com>
2026-03-27 17:33:58 -0300

    feat: upgrade to tailwind css v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -9,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
+    tailwindcss(),
     react(),
   ].filter(Boolean),
   resolve: {
