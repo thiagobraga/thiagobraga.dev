d9dd90f1a28f48f4d8027686618e3b1e8df11abe
Thiago Braga <thibraga06@gmail.com>
2026-04-01 19:45:00 -0300

    fix: update entry point and import global css for vite hmr

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import './index.css'
 import 'material-symbols/outlined.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
